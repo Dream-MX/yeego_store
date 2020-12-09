7578c821778d3bc261f112eeaa66be04e0647c7e
leixiang <530436057@qq.com>
2020-12-09 22:28:15 +0800

    新建模块测试

---
+++ start/src/test/java/com/yeego_store/start/StartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yeego_store.start;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
